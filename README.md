30bd96b659bfb872f414542910c86205fbce6d39
Ian Hess <ian.h.hess@gmail.com>
2022-10-14 14:30:32 -0400

    create files and learn basic dom manip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-dom-manip
+
+This is a repository for learning DOM manipulation in Odin Project
---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector("#container");
+const p = document.createElement("p");
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3";
+h3.style.color = "blue";
+
+const div = document.createElement("div");
+div.style.borderColor = "black";
+div.style.backgroundColor = "pink";
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+const p2 = document.createElement("p");
+p2.textContent = "ME TOO!";
+container.appendChild(p);
+container.appendChild(h3);
+
+div.appendChild(h1);
+div.appendChild(p2);
+container.appendChild(div);
+
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+const btn = document.querySelector("#btn");
+btn.onclick = alertFunction;
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", function (e) {
+  console.log(e.target);
+});
